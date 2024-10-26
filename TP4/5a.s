fbfa9e2f607d5a9790c20452c80bb1821c1b388c
ta <tomas-aragon@hotmail.com>
2024-10-26 14:03:22 -0300

    5a

---
+++ TP4/5a.s
@@ -0,0 +1,27 @@
+.data
+
+A: .word 1
+B: .word 3
+
+S: .word 0
+P: .word 0
+D: .word 0
+
+.code
+
+ld $t0, A($zero)
+ld $t1, B($zero)
+dadd $t0, $t1, $t0
+sd $t0, S($zero); S = A + B
+
+ld $t0, A($zero)
+dmul $t0, $t0, $t1; t0 := t0 * t1
+daddi $t0, $t0, 2
+sd $t0, P($zero)
+
+
+ld $t0, A($zero)
+dmul $t0, $t0, $t0
+daddi $t0, $t0, $t1
+sd $t0, D($zero); D = A^2 / B
+
